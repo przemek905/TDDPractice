206c8d86da843ac8144cf7f868ed7dba0db2cc4a
przemek905 <przemek920@wp.pl>
2019-10-20 20:03:52 +0200

    Refactor

--- src/main/java/fizzBuzz/FizzBuzzService.java
+++ src/main/java/fizzBuzz/FizzBuzzService.java
@@ -1,36 +1,40 @@
 package fizzBuzz;
 
+import java.util.stream.IntStream;
+
 class FizzBuzzService {
 
     static String[] doFizzBuzz(int from, int to) {
+        checkRangeParameters(from, to);
+
+        return IntStream.rangeClosed(from, to)
+                .mapToObj(FizzBuzzService::getResultForNumber).toArray(String[]::new);
+    }
+
+    private static String getResultForNumber(int i) {
+        if (i == 0) {
+            return String.valueOf(0);
+        }
+        else if (i % 3 == 0 & i % 5 == 0) {
+            return "FizzBuzz";
+        }
+        else if (i % 5 == 0) {
+            return "Buzz";
+        }
+        else if (i % 3 == 0) {
+            return "Fizz";
+        }
+        else {
+            return String.valueOf(i);
+        }
+    }
+
+    private static void checkRangeParameters(int from, int to) {
         if (from > to) {
             throw new IllegalStateException("Range is invalid. Start number is higher than ending.");
         }
         if (from < 0) {
             throw new IllegalStateException("FizzBuzz supports only positive numbers");
         }
-
-        String[] result = new String[to - from + 1];
-        int j = 0;
-
-        for (int i = from; i <= to; i++) {
-            if (i == 0) {
-                result[j] = String.valueOf(0);
-            }
-            else if (i % 3 == 0 & i % 5 == 0) {
-                result[j] = "FizzBuzz";
-            }
-            else if (i % 5 == 0) {
-                result[j] = "Buzz";
-            }
-            else if (i % 3 == 0) {
-                result[j] = "Fizz";
-            }
-            else {
-                result[j] = String.valueOf(i);
-            }
-            j++;
-        }
-        return result;
     }
 }
